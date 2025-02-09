bba078d0ecf418b83d3bc76295b2049b093e1efc
DekuFoo <ggmaster039@gmail.com>
2025-02-09 10:46:20 +0300

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="flex bg-cyan-900 items-center text-center justify-center">
+      123
+    </div>
+  )
+}
+export default App
---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+const startApp = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+startApp();
